ed80a06dfc65a1fd2ffbe74d92c741aaf06a3ddb
anilram <anilram.laxmisetti@qison.com>
2020-05-23 16:18:55 +0530

    3 files modified

--- src/app/autocomplete/autocomplete.component.ts
+++ src/app/autocomplete/autocomplete.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 export class Concept{
   id:number;
   name:string;
 }
 
+export class Authentication{
+  username:string;
+  password:string;
+}
 
 @Component({
   'selector':'app-autocomplete',
@@ -14,14 +19,25 @@ export class Concept{
 export class AutocompleteComponent implements OnInit {
   //public concepts:Map<string,string> = new Map([["1","Component"],["2","Service"]]);
   public conceptValue:string;
+  public authentication:Authentication;
+  public loginForm:FormGroup;
+  public submitted:boolean= false;
   public concepts:Array<Concept> = [{id:1,name:'Module'},
   {id:2,name:'Component'},{id:3,name:'Service'}
   ]
  
-  constructor() { }
+  constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit() {
     this.conceptValue='';
+    this.authentication = new Authentication();
+    this.authentication.username='';
+    this.authentication.password='';
+
+    this.loginForm = this.formBuilder.group({
+      username:['',Validators.required],
+      password:['',Validators.required]
+    });
     //this.concepts.set("3","Router");
     //this.concepts.set("4","Module");
 
@@ -31,6 +47,15 @@ export class AutocompleteComponent implements OnInit {
     let name:string=e.target.value;
     let id = this.concepts.filter(c=>c.name === name)[0];
     alert(id.id);
+  }
+
+  get f(){
+    return this.loginForm.controls;
+  }
+
+  doLogin(){
+    this.submitted = true;
+return ;
   }
 
 }
